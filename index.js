d3ee1e8aab67c7597289f47a9ad8b3019eb48cd0
Yaroslav Kosovych <ykosovy@softserveinc.com>
2023-01-13 10:48:48 +0200

    add static files

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const http = require('http');
 const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+
+const shopRouter = require('./routes/shop');
+const adminRouter = require('./routes/admin');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log(1);
-  res.send('<h1>1</h1>');
-  next();
-});
+app.use(parser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/add', (req, res, next) => {
-  console.log(2);
-  res.send('<h1>2</h1>');
-  next();
-});
+app.use(shopRouter);
 
+app.use('/admin', adminRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'))
+})
 
 app.listen(3000);
